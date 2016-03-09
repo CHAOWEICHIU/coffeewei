4169b300fee088fee9c5ec721fad077284ed462d
CHAOWEICHIU <chaoweichiu@gmail.com>
2016-03-09 08:56:04 -0500

    Edit A litte

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,6 @@ class Comment < ActiveRecord::Base
 	# validates :qty, presence: true, numericality: { only_integer: true }
 
 	validates_numericality_of :qty, :only_integer => true, :allow_nil => false, 
-    :greater_than_or_equal_to => 1,
-    :message => "can only be whole number between 1 and 50."
+    :greater_than_or_equal_to => 1
+    # :message => "can only be whole number between 1 and 50."
 end
