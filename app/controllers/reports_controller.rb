c44f1c5a2a1e5b0ee88b897b71dd84292b799fec
CHAOWEICHIU <chaoweichiu@gmail.com>
2016-03-10 13:20:04 -0500

    Add report for individual

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
-  	@users = User.all.sort
+  	@users = User.all.order("name ASC")
   	@order_total_count_array = [] 
   	@user_total_count_array = []
 	@users.each do |user|
@@ -25,13 +25,24 @@ class ReportsController < ApplicationController
   end
 
   def show
-  	@try = Hash.new
-  	@users = ["a","b","c"]
-  	
-  	@users.each do |h|
-  		@try[h] = [2,2]
+  	@user = User.find(params[:id])
+  	@orders = Order.where(:user_id => @user)
+  	@orders = @orders.order("visibility ASC")
+  	@order_total_hash = {}
+  	@orders.each do |order|
+		@comments = Comment.where(:order_id => order.id)
+		
+		@order_total_count = 0
+		@comments.each do |comment|
+			@menu_id = comment.menu_id
+			@comment_qty = comment.qty
+			@menu_id_price = Menu.find(@menu_id).price
+			@order_total_count = @order_total_count + (@comment_qty * @menu_id_price)
+		end
+	 						
+		@order_total_hash.store("#{order.id}", "#{@order_total_count}")
   	end
-  	
+
   
 
 
